773d809b8eafa725805f3aafeedcf8e55707686d
Joao Schmoeller <joaovitorgarciaschmo@gmail.com>
2023-01-19 21:02:17 -0300

    Primeiro commit

---
+++ secret-word/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = ({startGame}) => {
+  return (
+    <div>
+        <h1>Secret Word</h1>
+        <p>Clique no botão abaixo para iniciar o jogo</p>
+        <button onClick={startGame}>Começar o Jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen;
